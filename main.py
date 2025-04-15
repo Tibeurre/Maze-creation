f4b712e5260e5af8393dd0f99665991dbf954fe5
Antoine Jonquières <jonquieres.antoine.t8@dc.tohoku.ac.jp>
2025-04-15 16:21:40 +0900

    Added main.py, affichage.py(empty)

---
+++ main.py
@@ -0,0 +1,78 @@
+import random as rd
+import numpy as np 
+
+def generate(n =10):
+    maze = init(n)
+    start,end = exits(n)
+
+    maze[start], maze[end] = 2,3
+
+    maze = fill(maze,start,end)
+
+    maze[start], maze[end] = 2,3
+
+    return maze
+
+def init(n):
+    return np.zeros((n,n), dtype=int)
+
+def exits(n):
+    if head_or_tail():
+        start = (0, rd.randint(0,n-1))
+    else :
+        start = (rd.randint(0,n-1),0)
+    
+    if head_or_tail():
+        exit = (n-1,rd.randint(0,n-1))
+    else :
+        exit = (rd.randint(0,n-1),n-1)
+
+    while exit == start :
+        print(rd.randint(0,n-1))
+        exit = (rd.randint(0,n-1),rd.randint(0,n-1))
+    return start,exit
+
+def fill(maze,start,end):
+    current = start
+    while current != end :
+        available = free(maze,current)
+        current = available[rd.randint(0,len(available)-1)]
+        maze[current] = 1
+    return maze
+
+def free(maze,current):
+    available = []
+
+    while available == [] : #If no positions to "dig" : go to a random place "digged" and do it again
+
+        # Determining the available positions to "dig"
+        x, y = current
+        neighbors = []
+
+        if x > 0 :
+            neighbors.append((x - 1, y))
+        if x < maze.shape[0]-1 :
+            neighbors.append((x + 1, y))
+        if y > 0 :
+            neighbors.append((x, y - 1))
+        if y < maze.shape[1]-1 :
+            neighbors.append((x, y + 1))
+
+        for el in neighbors : 
+            if maze[el] == 3 :
+                return [el] #priority to the exit
+            elif maze[el] == 0:
+                available.append(el)
+        
+        if available ==[] :
+            #Put current at a random place if nowhere to dig
+            one_list = [tuple(pos) for pos in np.argwhere(maze==1)]
+            current = one_list[rd.randint(0,len(one_list)-1)]
+
+    return available
+
+def head_or_tail():
+    return rd.randint(0,1)
+
+
+print(generate())
